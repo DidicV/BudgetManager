6788ab3af3eb94df2d4446414fed71b3aced3978
Victor Didic <75629328+DidicV@users.noreply.github.com>
2023-12-09 19:04:14 +0200

    Innitial commit & gitignore

---
+++ BudgetManager/BudgetManager/BudgetManager/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using BudgetManager.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace BudgetManager.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
